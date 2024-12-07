05eb7c3edbb0e7dc25fcd87282bbf31199851013
Anna <115356891+anna-ricci@users.noreply.github.com>
2024-12-07 13:03:59 +0100

    Add files via upload

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from 'react';
+import "./Settings.css";
 
 const Settings = () => {
+
     return (
-        <div>Settings</div>
-    )
+        <div>
+            <div className="settings-container">
+                {/* Settings Label */}
+                <div>
+                <h1> Settings </h1></div>
+                
+                {/* Account Button */}
+                <div className="button-container">
+                    <button className="settings-button">
+                        {"Account"}
+                    </button>
+                </div>
+                {/* Notifications Button */}
+                <div className="button-container">
+                    <button className="settings-button">
+                        {"Notifications"}
+                    </button>
+                </div>
+                {/* Connected DevicesButton */}
+                <div className="button-container">
+                    <button className="settings-button">
+                        {"Connected Devices"}
+                    </button>
+                </div>
+                {/* Language Button */}
+                <div className="button-container">
+                    <button className="settings-button">
+                        {"Language"}
+                    </button>
+                </div>
+                {/* Accessibility Button */}
+                <div className="button-container">
+                    <button className="settings-button">
+                        {"Accessibility"}
+                    </button>
+                </div>
+                {/* Help button */}
+                <div className="button-container">
+                    <button className="settings-button">
+                        {"Help"}
+                    </button>
+                    </div>
+            </div>
+        </div>
+    );
 }
 
-export default Settings
+export default Settings;
