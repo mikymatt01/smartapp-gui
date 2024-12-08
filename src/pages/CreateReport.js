e953be93778d1492adb1f8cc422c461d750b3c7a
Anna <annabluricci@gmail.com>
2024-12-08 21:16:02 +0100

    Site SMO

--- src/pages/CreateReport.js
+++ src/pages/CreateReport.js
@@ -193,9 +193,9 @@ const CreateReport = () => {
               <option value="" disabled>
                 Select Site
               </option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
+              <option value="0">1</option>
+              <option value="1">2</option>
+              <option value="2">3</option>
             </select>
           </div>
         )
